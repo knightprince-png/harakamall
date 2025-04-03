2fc3ff4f2ea4374d9beb04dbfb04addabb512b99
knightprince-png <mwaurafelix754@gmail.com>
2025-04-03 12:03:21 +0300

    third commit

--- app/src/main/java/com/felix/harakamall/ui/Screens/item/itemscreen.kt
+++ app/src/main/java/com/felix/harakamall/ui/Screens/item/itemscreen.kt
@@ -1,7 +1,11 @@
 package com.felix.harakamall.ui.Screens.item
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Notifications
@@ -13,8 +17,16 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.felix.harakamall.R
 import com.felix.harakamall.ui.theme.blue
 import com.felix.harakamall.ui.theme.white
 
@@ -48,9 +60,30 @@ fun ItemScreen(){
                 }
                 }
         )//end
+
+        Image(painter = painterResource(R.drawable.img),
+            contentDescription = "img",
+            modifier = Modifier.fillMaxWidth().height(200.dp),
+            contentScale = ContentScale.FillWidth
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+
+        //search bar
+        var search by remember { mutableStateOf("") }
+
+
+
+
+        //end of search bar
+
     }
 
 }
+
+
+
+
+
 @Preview(showBackground = true)
 @Composable
 fun ItemScreenPreview(){
